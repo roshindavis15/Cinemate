04471f53e03890b9e598deb830be83e579125895
roshin <roshindavis15@gmail.com>
2024-05-14 14:16:39 +0530

    context and reducer implimented

--- src/context/movie-context.tsx
+++ src/context/movie-context.tsx
@@ -54,5 +54,10 @@ export const MovieContext=createContext<{
 
 export const MovieProvider=({children}:MovieContextProps)=>{
     const[state,dispatch]=useReducer(MovieReducer,initialMovieState);
-    return <MovieContext.Provider value={{state,dispatch}}>children{}</MovieContext.Provider>
+    return (
+        <MovieContext.Provider value={{state,dispatch}}>
+            {children}
+            </MovieContext.Provider>
+    )
+    
 }
