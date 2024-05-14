593b92a24af2eebd6deb9ef56e4e3258243620fa
roshin <roshindavis15@gmail.com>
2024-05-14 11:01:44 +0530

    router provider wrapped with movieprovider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import {router} from "./routes";
+import { MovieProvider } from "./context/movie-context";
 
 const App=()=>{
-    return <RouterProvider router={router}/>
+    return
+    <MovieProvider>
+     <RouterProvider router={router}/>
+    </MovieProvider>
+     
 };
 
 export default App;
